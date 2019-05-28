a90341014dfd4726b1aae8b2e59de65c338a3a5e
JoonasPit <43230082+JoonasPit@users.noreply.github.com>
2019-05-28 10:14:50 +0300

    Create searchthrough.ps1
    Search files based off name and write key-words out in to a output file

---
+++ searchthrough.ps1
@@ -0,0 +1,16 @@
+Function searchthrough {
+        # What to search for
+        $whatfiles  = Read-Host -Prompt "Mitä tiedostoa etsitään"
+        $keyWord = Read-Host -Prompt "Mitä sanaa etsitään tiedostoista"
+        Write-Output "Katso kansiosta scanthrough.log"
+        
+Foreach ($kw in $keyWord)
+       {
+    Get-Childitem -Filter *$whatfiles* -Recurse | 
+    Select-String -Pattern "$kw" | 
+
+    Select line, Path,LineNumber,@{n='KeyWord';e={ $kw }
+
+    } | Out-File .\scanthrough.log
+} 
+}
