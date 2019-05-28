307da69595accad94a49c96cb5c270bdf2950551
JoonasPit <43230082+JoonasPit@users.noreply.github.com>
2019-05-28 12:14:18 +0300

    Update searchthrough.ps1

--- searchthrough.ps1
+++ searchthrough.ps1
@@ -1,6 +1,8 @@
 # Before excecution navigate to folder where you are searching through
 
-Function searchthrough {
+Function searchthrough 
+
+{
 
         # defines the output file name. This version creates the file in same folder. Change name and path if necessary
         $outputFile = "userbasedscanthrough.log"
@@ -11,15 +13,19 @@ Function searchthrough {
             # User confirms deletion
             $confirmbool = Read-Host -Prompt "Poistetaanko $outputFile (Y/N): "
             while (Test-Path $outputFile){
+                # Check if user wants to delete old file or rename file and make the output file have a new name
                 if($confirmbool -eq  "Y" -Or $confirmbool -eq "y"){
                     Remove-Item $outputFile
                 }
+                # Asks for new filename
                 elseif ($confirmbool -eq "N" -Or $confirmbool -eq "n") {
                     $othername = Read-Host -Prompt "Anna tiedostolle toinen nimi"
                     $outputFile = $othername
                 }
+                # Keeps the user in the loop so the program doesn't throw unnecessarry errors. 
+                # The error message here tells the user to select either y or n
                 else{
-                    Write-Error "Ei kelpaa"
+                    Write-Error "Ei kelpaa valitse joko Y tai N"
                      $confirmbool = Read-Host -Prompt "Poistetaanko $outputFile (Y/N): "
                 }
         }
@@ -27,6 +33,7 @@ Function searchthrough {
         # What to search for, First one asks for the file name or type eg. log, .txt etc...
         $whatfiles  = Read-Host -Prompt "Mitä tiedostoa etsitään"
         # Searches for a specific word from the file
+        # do-while function makes it so the user can't search for an empty "word"
         do {
         
         $keyWord = Read-Host -Prompt "Mitä sanaa etsitään tiedostoista"
@@ -35,9 +42,9 @@ Function searchthrough {
                 }
             }while($keyword -eq "")
         
-        
-       Write-Output "Katso $outputFile" 
-        
+        # What name the file has. 
+        Write-Output "Katso $outputFile" 
+        # Write to the new file
         Foreach ($kw in $keyWord)
             {
                 # Select new match 
@@ -46,7 +53,7 @@ Function searchthrough {
                 # What was searched, the line that was found, on which line this can be found, and path to file
                 Select @{n='KeyWord';e={ $kw }}, line, LineNumber , Path | 
 
-    # Output file alter name @ the $outputfile variable to get different files
-    Out-File .\$outputfile
-} 
+        # Output file alter name @ the $outputfile variable to get different files
+        Out-File .\$outputfile
+       } 
 }
